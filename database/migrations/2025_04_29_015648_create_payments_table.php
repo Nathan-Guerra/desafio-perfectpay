e9165e1b62a114d7df4a50dc7a8998edc0ab805f
Nathan Oliveira <nguerra123.ng@gmail.com>
2025-04-30 18:10:04 -0300

    feat: create migrations

---
+++ database/migrations/2025_04_29_015648_create_payments_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('customer_id')->constrained('customers');
+            $table->uuid('payment_uuid')->unique();
+            $table->string('asaas_payment_id');
+            $table->string('type');
+            $table->unsignedInteger('value');
+            $table->string('currency', 3);
+            $table->string('status');
+            $table->date('due_at');
+            $table->timestamps();
+        });
+    }
+};
