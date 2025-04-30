ae1d98ed00beca2851d8a54443ae8505f8c269e8
Nathan Oliveira <nguerra123.ng@gmail.com>
2025-04-30 19:44:22 -0300

    feat(Gateway): create seeder

---
+++ database/seeders/GatewaySeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Gateway;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class GatewaySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Gateway::query()
+            ->insert([
+                ['name' => 'Asaas', 'slug' => 'asaas', 'created_at' => now(), 'updated_at' => now()],
+            ]);
+    }
+}
